992b8c22bef518c3745900eaff9a74e55d6ea070
dongjianxiong <jianxiong20090919@126.com>
2022-12-14 22:30:29 +0800

    update version

--- LNCommonKit.podspec
+++ LNCommonKit.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'LNCommonKit'
-  s.version          = '0.1.7'
+  s.version          = '0.1.8'
   s.summary          = '基础库组件，用于基础服务层代码'
 
 # This description is used to generate tags and improve search results.
--- pod_script/pod_remote_check.sh
+++ pod_script/pod_remote_check.sh
@@ -1,11 +1,16 @@
 
 pwd
-version=pod_test_temp_tag
+version=$1
 echo "Test $version"
 git tag -d $version
 git push --delete origin $version
+echo "Begin create a tag $version"
 git tag $version
+echo "Begin push a tag $version"
 git push origin $version
+echo "Begin to lint $version"
 pod spec lint  --allow-warnings --verbose
+echo "End lint $version"
+echo "Begin to delete tag $version"
 git tag -d $version
 git push --delete origin $version
